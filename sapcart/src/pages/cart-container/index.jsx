9b6d9bd9d37718d4fe9129d42c261361aa252346
Asrar Abuchaer <asrarabuchaer@gmail.com>
2023-10-04 14:37:05 +0700

    new brand

---
+++ sapcart/src/pages/cart-container/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "@chakra-ui/react";
+import hoodie from "./image/hoodie";
+
+function CartContainer() {
+  return (
+    <Container>
+      <h1>Daftar Item</h1>
+      <img src="hoodie" alt="my-hoodie" />
+    </Container>
+  );
+}
+
+export default CartContainer;
