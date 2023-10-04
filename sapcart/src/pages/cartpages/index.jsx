8a78c31129c1abc3b668d12c7c3435ecd1b46cb8
I Putu Phillip Steven <putu.phillip@gmail.com>
2023-10-04 21:39:46 +0700

    update index.jsx

--- sapcart/src/pages/cartpages/index.jsx
+++ sapcart/src/pages/cartpages/index.jsx
@@ -1,16 +1,14 @@
-import { Box, Text, Button, Container } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 
 function CartPages() {
   return (
-    <Box display="flex" flexDir="column" bgColor="red" minH="100vh">
-      <Box bg="grey" p="1em" display="flex" flexDir="flex-start">
+    <Box display="flex" flexDir="column" minH="100vh">
+      <Box bg="grey" p="2.5em 1.5em" display="flex" flexDir="flex-start">
         <Box>
           <Text as="h1">Keranjang</Text>
         </Box>
       </Box>
-      <Box>
-          
-      </Box>
+      <Box></Box>
     </Box>
   );
 }
