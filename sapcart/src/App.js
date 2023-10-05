14fbe8eb4da50f733c39940b8880e98626e691dd
Asrar Abuchaer <asrarabuchaer@gmail.com>
2023-10-05 14:15:23 +0700

    update

--- sapcart/src/App.js
+++ sapcart/src/App.js
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 import CartContainer from "./cart-container";
 
+=======
+import CartContainer from "./pages/cart-container";
+>>>>>>> Stashed changes
 function App() {
   return (
     <div>
--- sapcart/src/pages/cart-container/index.jsx
+++ sapcart/src/pages/cart-container/index.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Box, Container } from "@chakra-ui/react";
 import 
 
@@ -5,6 +6,23 @@ function CartContainer() {
   return (
     
   );
+=======
+import sepatu from "./images/sepatu.jpeg";
+
+function CartContainer() {
+  return (
+    <div>
+      <h1>Daftar Item</h1>
+      <img src={sepatu} alt="sepatu" />
+    </div>
+  );
+
+  function flex() {
+    <flex>
+     <h1></h1>
+    </flex>;
+  }
+>>>>>>> Stashed changes
 }
 
 export default CartContainer;
