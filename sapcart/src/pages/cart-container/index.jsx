d7b512c2825573b99499fb34b47a519518a1b6c6
Asrar Abuchaer <asrarabuchaer@gmail.com>
2023-10-05 20:36:27 +0700

    update malam

--- sapcart/src/pages/cart-container/index.jsx
+++ sapcart/src/pages/cart-container/index.jsx
@@ -1,32 +1,18 @@
 import { Box, Text, VStack, Image, HStack, Heading } from "@chakra-ui/react";
 import img1 from "../../pages/cart-container/image/1.jpg";
+import Card from "./components/cards";
 import { BsFillCartPlusFill } from "react-icons/bs";
 function CartContainer() {
   return (
-    <Box p="2.5em 1.5em" backgroundColor={"lightgray"}>
-      <Box p="2.5em 1.5em">
+    <Box h="auto" w="full" p="2.5em 1.5em" backgroundColor={"lightgray"}>
+      <Box backgroundColor="red" p="2.5em 1.5em">
         <VStack>
-          <Box p="1.5em 2.5em">
+          <Box>
             <Heading>Daftar Item</Heading>
           </Box>
           <Box>
             <HStack spacing="24px">
-              <Box
-                borderRadius="1em"
-                w="240px"
-                h="320px"
-                p="2.5em 1.5em"
-                bg="blue.200"
-              >
-                <Box borderRadius="0.5em" overflow="hidden">
-                  <Image src={img1} />
-                </Box>
-                <VStack fontSize="sm" align="flex-end" spacing="0">
-                  <Text>Celana</Text>
-                  <Text>Rp 150.000,00-</Text>
-                  <BsFillCartPlusFill />
-                </VStack>
-              </Box>
+              <Card />
             </HStack>
           </Box>
         </VStack>
