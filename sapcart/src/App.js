70906755306f807dae5f8adef5e0d2650be2fb98
I Putu Phillip Steven <putu.phillip@gmail.com>
2023-10-05 20:28:48 +0700

    Update

--- sapcart/src/App.js
+++ sapcart/src/App.js
@@ -1,14 +1,14 @@
 import Navbar from "./pages/navbar";
 import JumboTron from "./pages/jumboTron";
 import CartPages from "./pages/cartpages";
-
+import { Container } from "@chakra-ui/react";
 function App() {
   return (
-    <>
+    <Container maxW="100vw" padding="0">
       <Navbar />
       <JumboTron />
       <CartPages />
-    </>
+    </Container>
   );
 }
 
